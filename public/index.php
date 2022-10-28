95464daca25ad2dc53af762a713907ca8e8b5a0b
FERRE24Mo <morgan.ferre85@gmail.com>
2022-10-28 10:52:12 +0200

    push tout

--- public/index.php
+++ public/index.php
@@ -3,8 +3,9 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-header('Access-Control-Allow-Origin: http://localhost:4200');
-// header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Origin: http://localhost:4200');
+header("Access-Control-Allow-Headers: *");
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
