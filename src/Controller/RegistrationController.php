ce2404b93a4222461bb054046ad1fd96a6763c46
GRELET-Axel <grelet.axel.85@gmail.com>
2022-10-31 12:30:45 +0100

    register route finie

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use App\Entity\Participant;
 use App\Entity\User;
+use App\Repository\CampusRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -14,26 +15,26 @@ use Symfony\Component\HttpFoundation\Response;
 class RegistrationController extends AbstractController
 {
     #[Route('/api/register', name: 'app_register')]
-    public function index(ManagerRegistry $doctrine,Request $request,UserPasswordHasherInterface $passwordHasher): Response
+    public function index(ManagerRegistry $doctrine,Request $request,UserPasswordHasherInterface $passwordHasher, CampusRepository $campusRepository): Response
     {
         // ... e.g. get the user data from a registration form
         $entityManager = $doctrine->getManager();
-        $content = $request->getContent();
-        var_dump($content);
+        $content = json_decode($request->getContent());
         $user = new Participant();
 
-        
-        // TODO with content
-        $user->setEmail('test@test.fr')
-        ->setRoles([])
-        ->setNom('testNom')
-        ->setPrenom('TestPrenom')
-        ->setTelephone('0123456789')
+        // $campus = $doctrine->getRepository(CampusRepository::class)->find($content->campus_id);
+        $campus = $campusRepository->find($content->campus_id);
+        $user->setEmail($content->email)
+        ->setRoles($content->administrateur?['ROLE_ADMIN']:['ROLE_USER'])
+        ->setNom($content->nom)
+        ->setPrenom($content->prenom)
+        ->setTelephone($content->telephone)
         ->setActif(false)
-        ->setAdministrateur(false)
+        ->setAdministrateur($content->administrateur?true:false)
+        ->setCampus($campus)
         ;
 
-        $plaintextPassword = "testmdp";
+        $plaintextPassword = $content->password;
 
 
         // hash the password (based on the security.yaml config for the $user class)
